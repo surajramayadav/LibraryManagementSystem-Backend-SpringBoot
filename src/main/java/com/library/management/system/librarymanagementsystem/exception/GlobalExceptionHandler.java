93f44e73d8e491a0b729d11342eeedac93a40aeb
indgenous <surajramayadav@gmail.com>
2022-02-10 18:22:57 +0530

    log4j2 implemented

--- src/main/java/com/library/management/system/librarymanagementsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/management/system/librarymanagementsystem/exception/GlobalExceptionHandler.java
@@ -7,16 +7,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     
+	final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+		
 
     // handling specific exception
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<Object> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request){
         ErrorMessage errorDetails = 
         new ErrorMessage(false, exception.getMessage(),request.getDescription(false), new Date());
+		logger.error(exception.getMessage());
 		return new ResponseEntity<Object>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 
@@ -26,6 +30,7 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<Object> globalExceptionHandling(Exception exception, WebRequest request){
 		ErrorMessage errorDetails = 
 				new ErrorMessage(false, exception.getMessage(),request.getDescription(false), new Date());
+				logger.error(exception.getMessage());
 		return new ResponseEntity<Object>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
